e7dfa5fd38b757ce36e3280c7f41419d698b5b08
Chulhee Lee <leefehee@naver.com>
2024-10-05 02:56:17 +0900

    test9
    Signed-off-by: Chulhee Lee <leefehee@naver.com>

--- contracts/BdcBPT.sol
+++ contracts/BdcBPT.sol
@@ -2,10 +2,7 @@ pragma solidity ^0.5.0;
 
 import "../node_modules/@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20Detailed.sol";
 import "../node_modules/@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20Mintable.sol";
-test
-test
-test
-test
+
 contract BdcBPT is
    ERC20Detailed, 
    ERC20Mintable
